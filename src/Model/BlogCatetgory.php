3a0eeebefb4ee682dde74c9688b907f2f009bf12
Ralph Slooten <axllent@gmail.com>
2021-08-19 10:21:40 +1200

    Fix blog category link when URL parameters are used.

--- src/Model/BlogCatetgory.php
+++ src/Model/BlogCatetgory.php
@@ -1,44 +1,90 @@
 <?php
-
 namespace Axllent\Weblog\Model;
 
 use Axllent\Weblog\Model\Blog;
 use Axllent\Weblog\Model\BlogPost;
-use SilverStripe\View\Parsers\URLSegmentFilter;
+use SilverStripe\Core\Convert;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBDatetime;
-use SilverStripe\Core\Convert;
 use SilverStripe\Security\Permission;
+use SilverStripe\View\Parsers\URLSegmentFilter;
 
 class BlogCategory extends DataObject
 {
+    /**
+     * Table name
+     *
+     * @var string
+     */
     private static $table_name = 'BlogCategory';
 
+    /**
+     * Default sort
+     *
+     * @var array
+     */
     private static $default_sort = [
-        'Title'
+        'Title',
     ];
 
+    /**
+     * Database field definitions.
+     *
+     * @var    array
+     * @config
+     */
     private static $db = [
-        'Title' => 'Varchar(100)',
-        'URLSegment' => 'Varchar(100)'
+        'Title'      => 'Varchar(100)',
+        'URLSegment' => 'Varchar(100)',
     ];
 
+    /**
+     * One-to-zero relationship defintions.
+     *
+     * @var    array
+     * @config
+     */
     private static $has_one = [
-        'Blog' => Blog::class
+        'Blog' => Blog::class,
     ];
 
-    private static $belongs_many_many = array(
+    /**
+     * Defines inverse side of a many-many relationship.
+     *
+     * @var    array
+     * @config
+     */
+    private static $belongs_many_many = [
         'BlogPosts' => BlogPost::class,
-    );
+    ];
 
+    /**
+     * Provides a default list of fields to be used by a 'summary'
+     * view of this object.
+     *
+     * @var    string
+     * @config
+     */
     private static $summary_fields = [
-        'Title', 'BlogPosts.Count'
+        'Title', 'BlogPosts.Count',
     ];
 
+    /**
+     * Field labels
+     *
+     * @var array
+     */
     private static $field_labels = [
-        'BlogPosts.Count' => 'Blog Posts'
+        'BlogPosts.Count' => 'Blog Posts',
     ];
 
+    /**
+     * Data administration interface in Silverstripe.
+     *
+     * @see    {@link ValidationResult}
+     * @return FieldList Returns a TabSet for usage within the CMS
+     */
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
@@ -49,47 +95,78 @@ class BlogCategory extends DataObject
             /* Remove Add New button */
             $config = $fields->dataFieldByName('BlogPosts')
                 ->getConfig();
+
             $config->removeComponentsByType(
-                $config->getComponentByType('SilverStripe\\Forms\\GridField\\GridFieldAddNewButton')
+                GridFieldAddNewButton::class
             );
         }
 
         return $fields;
     }
 
-    public function Link()
+    /**
+     * Link
+     *
+     * @return string
+     */
+    public function link()
     {
-        return $this->Blog()->Link() . 'category/' . $this->URLSegment . '/';
+        return $this->Blog()->Link('category/' . $this->URLSegment . '/');
     }
 
-    /* Return only valid blog posts > PublishDate */
+    /**
+     * Return only valid blog posts > PublishDate
+     *
+     * @return DataList
+     */
     public function getVisibleBlogPosts()
     {
         return $this->Blogposts()
-            ->where(sprintf('"PublishDate" < \'%s\'', Convert::raw2sql(DBDatetime::now())));
+            ->where(
+                sprintf(
+                    '"PublishDate" < \'%s\'',
+                    Convert::raw2sql(DBDatetime::now())
+                )
+            );
     }
 
+    /**
+     * Validate the current object.
+     *
+     * @see    {@link ValidationResult}
+     * @return ValidationResult
+     */
     public function validate()
     {
-        $valid = parent::validate();
+        $valid       = parent::validate();
         $this->Title = trim($this->Title);
 
         $filter = new URLSegmentFilter;
 
         $this->URLSegment = $filter->filter($this->Title);
 
-        $exists = $this->Blog()->Categories()->filter([
-            'URLSegment' => $this->URLSegment
-        ])->exclude('ID', $this->ID)->count();
+        $exists = $this->Blog()->Categories()
+            ->filter('URLSegment', $this->URLSegment)
+            ->exclude('ID', $this->ID)
+            ->count();
 
         if (!$this->Title) {
             $valid->addError('Enter a category name');
         } elseif ($exists) {
             $valid->addError('A category named ' . $this->Title . ' already exists');
         }
+
         return $valid;
     }
 
+    /**
+     * Permissions canEdit
+     *
+     * @param Member $member  SilverStripe member
+     * @param array  $context Array
+     *
+     * @return boolean
+     */
     public function canEdit($member = null, $context = [])
     {
         $extended = $this->extendedCan('canEdit', $member);
@@ -99,9 +176,18 @@ class BlogCategory extends DataObject
         if (Permission::check('CMS_ACCESS_Weblog', 'any', $member)) {
             return true;
         };
+
         return parent::canEdit($member, $context);
     }
 
+    /**
+     * Permissions canView
+     *
+     * @param Member $member  SilverStripe member
+     * @param array  $context Array
+     *
+     * @return boolean
+     */
     public function canView($member = null, $context = [])
     {
         $extended = $this->extendedCan('canView', $member);
@@ -111,9 +197,18 @@ class BlogCategory extends DataObject
         if (Permission::check('CMS_ACCESS_Weblog', 'any', $member)) {
             return true;
         };
+
         return parent::canView($member, $context);
     }
 
+    /**
+     * Permissions canDelete
+     *
+     * @param Member $member  SilverStripe member
+     * @param array  $context Array
+     *
+     * @return boolean
+     */
     public function canDelete($member = null, $context = [])
     {
         $extended = $this->extendedCan('canDelete', $member);
@@ -123,6 +218,7 @@ class BlogCategory extends DataObject
         if (Permission::check('CMS_ACCESS_Weblog', 'any', $member)) {
             return true;
         };
+
         return parent::canDelete($member, $context);
     }
 }
